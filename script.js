449f2a8252535fb6c9f47141010556c3b8bb21d7
Camille Dent <camilledent995@gmail.com>
2022-06-12 10:53:36 -0400

    Add instructions card html, styles, and event listeners

--- script.js
+++ script.js
@@ -2,11 +2,13 @@ const startCard = document.getElementById('start-text');
 const daysCard = document.getElementById('days-text');
 const cupsCard = document.getElementById('cups-text');
 const resultCard = document.getElementById('result-text');
+const instructionsCard = document.getElementById('instructions-text');
 
 const startButton = document.getElementById('get-started');
 const nextFromDaysButton = document.getElementById('next-from-days');
 const nextFromCupsButton = document.getElementById('next-from-cups');
 const nextFromResultButton = document.getElementById('next-from-result');
+const returnToStartButton = document.getElementById('return-to-start');
 
 const threeDaysButton = document.getElementById('three-days');
 const fourDaysButton = document.getElementById('four-days');
@@ -16,6 +18,7 @@ const largeCupsButton = document.getElementById('large-cup');
 
 const coffeeResult = document.getElementById('coffee-calc');
 const waterResult = document.getElementById('water-calc');
+const concentrateResult = document.getElementById('half-cup-size');
 
 startButton.addEventListener('click', () => {
     startCard.style.display = "none";
@@ -32,4 +35,12 @@ nextFromCupsButton.addEventListener('click', () => {
     resultCard.style.display = "block";
 });
 
-// nextFromResultButton.addEventListener('click', () => {});
+nextFromResultButton.addEventListener('click', () => {
+    resultCard.style.display = "none";
+    instructionsCard.style.display = "block";
+});
+
+returnToStartButton.addEventListener('click', () => {
+    instructionsCard.style.display = "none";
+    startCard.style.display = "block";
+})
